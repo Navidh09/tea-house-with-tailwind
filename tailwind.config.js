a1319619be331f166959f627427a54bd9e0dca39
Navid Hasan <navid.nyon@gmail.com>
2024-10-27 00:44:49 +0600

    footer section all done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+            'hero-pattern': "url('/images/bg_cup.png')"
+        }
+      },
     },
     plugins: [],
   }
